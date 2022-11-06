b8df3739726d8834a7cb229da68a3b12688e17ef
Ashna PS <77959009+ashnaps@users.noreply.github.com>
2022-11-06 07:31:35 +0530

    Update scripts
    updated functions

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // 1. Create two variables, firstCard and secondCard. 
 // Set their values to a random number between 2-11
 // 2. Create a variable, sum, and set it to the sum of the two cards
-
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
 let messageEl=document.getElementById("message-el");
 let sumEl=document.getElementById("sum-el");
-let cardsEl=document.querySelector("#cards-el");
-let firstCard=getRandomCard();
-let secondCard=getRandomCard();
-let cards=[firstCard, secondCard]
-let sum = firstCard+secondCard;
+let cardsEl=document.getElementById("cards-el");
+
+
 //console.log(sum);
 
 // if (sum<21){
@@ -43,45 +45,62 @@ else{
 //     greetingEl.textContent += sentence[i] + " "
 // }
 
+function getRandomCard() {
+    let randomNumer = Math.floor( Math.random()*13 ) + 1
+    if (randomNumer > 10) {
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+        return randomNumer
+    }
+   // return Math.floor(Math.random() * 13) + 1
+}
+
+function startGame(){
+    isAlive = true
+    let firstCard=getRandomCard();
+    let secondCard=getRandomCard();
+    cards=[firstCard, secondCard]
+    sum = firstCard+secondCard;
+    renderGame();
+}
 
 function renderGame(){
     let message;
-
     cardsEl.textContent="Cards: ";
     for(let i=0;i<cards.length;i++)
     {
         cardsEl.textContent +=cards[i]+" ";
     }
+    sumEl.textContent="Sum: "+sum;
 
 if(sum<=20){
     message="Do you want to draw a new card? 🙂";
 }
 else if(sum===21){
     message="Wohoo! You've got Blackjack! 🥳";
+    hasBlackJack = true
 }
 else{
     message="You're out of the game! 😭";
+    isAlive = false
 }
     messageEl.textContent=message;
-    sumEl.textContent="Sum: "+sum;
+    
     //carsdEl.textContent="Card: "+firstCard+" "+secondCard;
     //carsdEl.textContent="Card: "+cards[0]+" "+cards[1];
     
 }
-function getRandomCard() {
-    return Math.floor(Math.random() * 13) + 1
+
+function newCard() {
+    // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
+
 }
 
-
-
-function newCard(){
-    let card=getRandomCard();
-    cards.push(card);
-    console.log(cards);
-    sum=sum+card;
-    renderGame();
-}
-
-function startGame(){
-    renderGame();
-}
